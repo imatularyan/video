f83baaf14ff99227597d62b36f491d9b3fc48dd6
Atul Aryan <imatularyan@gmail.com>
2023-02-23 03:09:18 +0530

    optimization

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
-
-const btn = document.querySelector('.switch-btn');
+const btn = document.querySelector('.switch');
 const video = document.querySelector('.video-container');
+const layer = document.querySelector('#layer');
 
-btn.addEventListener('click', function () {
+btn.addEventListener('input', function () {
     if (!btn.classList.contains('slide')) {
         btn.classList.add('slide');
+        layer.classList.add('screen')
         video.pause();
     }
     else {
+        layer.classList.remove('screen');
         btn.classList.remove('slide');
         video.play();
     }
